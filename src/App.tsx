3177b75a96ade207fa535feba5b6ddab66fa4a3a
geek <ts.analista@gmail.com>
2026-04-21 22:52:51 -0300

    feat: add flexbox card info

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
 import './App.css'
-
+import logo from './assets/hero.png'
 function App() {
 
   return (
     <>
+      <main className="profile-cards">
+        <section className="profile-cards__card">
+          <img src={logo} alt="" className="profile-cards__card--img"/>
+            <div className="profile-cards__card--info">
+                <p className="profile-cards__card--info__name">
+                  Alex Front End
+                </p>
+                <p className="profile-cards__card--info__role">
+                    CFO
+                </p>
+            </div>
+        </section>
 
+          <section className="profile-cards__card">
+              <img src={logo} alt="" className="profile-cards__card--img"/>
+              <div className="profile-cards__card--info">
+                  <p className="profile-cards__card--info__name">
+                      Alex Front End
+                  </p>
+                  <p className="profile-cards__card--info__role">
+                      CFO
+                  </p>
+              </div>
+          </section>
+
+          <section className="profile-cards__card">
+              <img src={logo} alt="" className="profile-cards__card--img"/>
+              <div className="profile-cards__card--info">
+                  <p className="profile-cards__card--info__name">
+                      Alex Front End
+                  </p>
+                  <p className="profile-cards__card--info__role">
+                      CFO
+                  </p>
+              </div>
+          </section>
+      </main>
     </>
   )
 }
